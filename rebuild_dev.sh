9949169494760f4e9b069141364b5b32935aeafb
Daan B <daan.w.beltman@gmail.com>
2021-11-01 23:47:05 +0100

    improve localdev script

--- rebuild_dev.sh
+++ rebuild_dev.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
 owner=dockerdaan
 image=mountnsync
+
+backuppath=$1
+containername=$2
+
 docker build -t $owner/$image:dev -f Dockerfile .
-docker run -v /var/run/docker.sock:/var/run/docker.sock $owner/$image:dev
+docker run -v /var/run/docker.sock:/var/run/docker.sock $owner/$image:dev --archive --backup all --destination $backuppath $containername
+
