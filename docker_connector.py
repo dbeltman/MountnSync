1de53b538336742cd5ef61b5e8db62d7bbcf78d9
Daan B <daan.w.beltman@gmail.com>
2021-11-06 03:08:06 +0100

    add service support, yaml config

--- docker_connector.py
+++ docker_connector.py
@@ -1,7 +1,42 @@
-import docker
+import docker, yaml
+from docker.api import container
 from datetime import datetime
+from os import getenv
+from time import sleep
+import json
 
 d = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+# Read YAML file
+with open("config.yaml", 'r') as stream:
+    config = yaml.safe_load(stream)
+
+def listservices():
+    list = d.services.list()
+    return list
+
+def getserviceidbyname(service_name):
+    list = listservices()
+    for service in list:
+        if service.name == service_name:
+            service_id = service.id
+    return service_id
+
+def getservicedetails(service_name):
+    service_id = getserviceidbyname(service_name)
+    service_details = d.services.get(service_id).tasks()
+    # service_attrs = d.services.get(service_id).attrs
+    # print(json.dumps(service_attrs, indent=4, sort_keys=True))
+    return service_details
+
+def getservicestate(service_name):
+    service_details = getservicedetails(service_name)
+    for task in service_details:
+        if task['Status']['Message'] != "shutdown":
+            service_state = "running"
+            return service_state
+    service_state = "shutdown"
+    return service_state
 
 def listcontainers():
     list = d.containers.list()
@@ -11,17 +46,41 @@ def containerdetails(name):
     details = d.containers.get(name)
     return details
 
-def getContainerMounts(c_id):
-    mounts = d.containers.get(c_id).attrs['Mounts']
+def getContainerMounts(c_id, type):
+    if type == 'container':
+        mounts = d.containers.get(c_id).attrs['Mounts']
+    elif type == 'service':
+        mounts = d.services.get(getserviceidbyname(c_id)).attrs['Spec']['TaskTemplate']['ContainerSpec']['Mounts']
+        print(mounts)
+    else: 
+        print("No (valid) type given")
+        exit(1)
     return mounts
 
-def containerCtl(c_id, action):
+def containerCtl(c_id, action, type):
     try:
         print(action + " container " + c_id)
-        if action == 'stop':
-            d.containers.get(c_id).stop()
-        elif action == 'start':
-            d.containers.get(c_id).start()
+        if type == 'container':
+            if action == 'stop':
+                d.containers.get(c_id).stop()
+            elif action == 'start':
+                d.containers.get(c_id).start()
+        elif type == 'service':
+            if action == 'stop':
+                d.services.get(getserviceidbyname(c_id)).scale(0)
+                timeout = 0
+                while getservicestate(c_id) == 'running':
+                    if timeout < 30:
+                        sleep(1)
+                        print(c_id + " still " + getservicestate(c_id))
+                        timeout += 1
+                    else: 
+                        print("Timed out waiting for " + c_id + " to stop")
+            elif action == 'start':
+                d.services.get(getserviceidbyname(c_id)).scale(1)
+        else:
+            print("No (valid) type given!")
+            exit(1)
     except Exception as exception:
         print("Could not " + action + " container")
         print(exception)
@@ -29,25 +88,36 @@ def containerCtl(c_id, action):
 
 
 def makeBackup(hostSource, hostDestination, containerSource, containername):
-    hostBackupDestination=hostDestination+"/"+containername+"/mounts"+containerSource
-    print("Backing up " + hostSource + " To " + hostBackupDestination +"/..")
-    volumeconfig={
-        hostSource: {'bind': '/source', 'mode': 'ro'},
-        hostBackupDestination: {'bind': '/destination', 'mode': 'rw'}
-        }
-    try:
-        backupcontainer = d.containers.run(
-        "rclone/rclone",  
-        "copy -P /source /destination",
-        volumes=volumeconfig,
-        name='mountnsync-rclone',
-        remove=True,
-        )
-        print(backupcontainer)
-    except Exception as exception:
-        print(exception)
-        print("Failed to run rclone container!")
-        exit(1)
+    if containername in config:
+        print("App config found for " + containername + " !")
+        ignoredmounts = config[containername]['ignoredmounts']
+        print(ignoredmounts)
+    else:
+        print("No app config found for " + containername + " !")
+        ignoredmounts = []
+
+    if hostSource in ignoredmounts or containerSource in ignoredmounts:
+        print("Skipping backup of mount" + hostSource + ":" + containerSource + " as it was found in ignoredmounts list..")
+    else:
+        hostBackupDestination=hostDestination+"/"+containername+"/mounts"+containerSource
+        print("Backing up " + hostSource + " To " + hostBackupDestination +"/..")
+        volumeconfig={
+            hostSource: {'bind': '/source', 'mode': 'ro'},
+            hostBackupDestination: {'bind': '/destination', 'mode': 'rw'}
+            }
+        try:
+            backupcontainer = d.containers.run(
+            "rclone/rclone",  
+            "copy -P /source /destination",
+            volumes=volumeconfig,
+            name='mountnsync-rclone',
+            remove=True,
+            )
+            print(backupcontainer)
+        except Exception as exception:
+            print(exception)
+            print("Failed to run rclone container!")
+            exit(1)
 
 def archiveBackup(containername, backuppath, archivepath):
     archivename=containername+"-"+datetime.today().strftime('%Y-%m-%d-%H%M%S')+".tar.gz"
--- main.py
+++ main.py
@@ -1,17 +1,52 @@
+from os import wait, getenv
+from time import sleep
 import docker_connector, json, argparse, sys
-# Args
+
+# Main Args
 parser = argparse.ArgumentParser()
 parser.add_argument("container", type=str, metavar='container', help="Name or ID of Container")
 parser.add_argument("-b", "--backup", type=str, help = "Make a backup of the container mount(s), usage: 'all' or </mount/in/target/container> (todo)")
 parser.add_argument("-d", "--destination", type=str, help = "/host/destination/path")
+parser.add_argument("-t", "--type", type=str, help = "Container type, usage: 'service' or 'container'")
+
+# Secondary (test) args
 parser.add_argument("-i", "--details", type=str, help = "Get detail of Container, usage: 'all', 'mounts'")
+parser.add_argument("-l", "--list", type=str, help = "List either 'containers' or 'services'")
+parser.add_argument("-s", "--scale", type=str, help = "Scale Service")
+
 # Flags
 parser.add_argument("-a", "--archive", action='store_true', help="Archive the backup location afterwards. /destination/<containername>/all/your/data will be stored in /destination/<containername><todaysdate>.tar.gz")
+parser.add_argument("-sd", "--servicedetails", action='store_true', help="Show service details of the given container")
 args= parser.parse_args()
 
-if args.container == "all":
-    list = docker_connector.listcontainers()
+# Timeout in seconds to wait for the container/service to stop running
+stop_timeout = getenv('STOPTIMEOUT', "30")
+
+if args.list == "services":
+    list = docker_connector.listservices()
+    for service in list:
+        print(str(service.name))
+        print(json.dumps(service.attrs[''], indent=4, sort_keys=True))
     print(str(list))
+elif args.servicedetails:
+    details =  docker_connector.getservicedetails(args.container)
+    # print(json.dumps(details, indent=4, sort_keys=True))
+elif args.scale:
+    service_id = docker_connector.getserviceidbyname(args.container)
+    test = docker_connector.scaleservice(service_id, int(args.scale))
+    print(test)
+    service_state = docker_connector.getservicestate(args.container)
+    print(service_state)
+    timeout = 0
+    while docker_connector.getservicestate(args.container) == 'running':
+        if timeout < int(stop_timeout):
+            sleep(1)
+            service_state = docker_connector.getservicestate(args.container)
+            print(service_state)
+            timeout += 1
+        else:
+            print("Timed out waiting for " + args.container + " to scale down.")
+            exit(1)
 
 elif args.details:
     if args.details == 'all':
@@ -20,19 +55,34 @@ elif args.details:
         print(str(detailsample))
     elif args.details == 'mounts':
         print(str(args.container))
-        mounts = docker_connector.getContainerMounts(args.container)
+        mounts = docker_connector.getContainerMounts(args.container, args.type)
         for mount in mounts:
-            print(mount['Source'] + ":" + mount['Destination'])
+            if args.type == 'container':
+                print(mount['Source'] + ":" + mount['Destination'])
+            elif args.type == 'service':
+                #TODO named volume support
+                print(mount['Source'] + ":" + mount['Target'])
 
 elif args.backup:
     if args.destination:
-        docker_connector.containerCtl(args.container, "stop")
+        # Stop our container or scale down service
+        docker_connector.containerCtl(args.container, "stop", args.type)
         if args.backup == 'all':
-            mounts = docker_connector.getContainerMounts(args.container)
+            mounts = docker_connector.getContainerMounts(args.container, args.type)
+            print(mounts)
             for mount in mounts:
-                docker_connector.makeBackup(mount['Source'], args.destination, mount['Destination'], args.container)
-        docker_connector.containerCtl(args.container, "start")
+                if args.type =='container':
+                    docker_connector.makeBackup(mount['Source'], args.destination, mount['Destination'], args.container)
+                elif args.type == 'service':
+                    #TODO named volume support
+                    print("Backing up " + mount['Source'] + " to " + args.destination)
+                    docker_connector.makeBackup(mount['Source'], args.destination, mount['Target'], args.container)
+                else:
+                    print("No (valid) type given")
+        # Start our container or scale up service
+        docker_connector.containerCtl(args.container, "start", args.type)
         if args.archive == True:
+            print("Archiving backup")
             archivePath=args.destination+"/"+args.container+"/archives"
             backupPath=args.destination+"/"+args.container+"/mounts"
             docker_connector.archiveBackup(args.container, backupPath, archivePath)
